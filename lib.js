c6dcbfabf0b4edfd7b577ea6cd4cc28690389dd3
Rudraksh MK <rudraksh@paperkite.in>
2017-03-15 00:24:59 +0530

    Added a function to get a random agent from a particular country.

--- lib.js
+++ lib.js
@@ -2,6 +2,7 @@ const got = require('got')
 const Promise = require('bluebird')
 
 // This function grabs a random agent.
+// To call: var a; ppl.getAgent().then(function(res){a = res})
 function getAgent(){
     return new Promise(function(resolve, reject){
         got('http://pplapi.com/random.json')
@@ -11,5 +12,17 @@ function getAgent(){
     })
 }
 
+// This function accepts a valid two-letter country code, and then grabs an agent from that country.
+// To call: var a; ppl.getAgentByCountry('ca').then(function(res){a = res})
+function getAgentByCountry(code){
+    return new Promise(function(resolve, reject){
+        got(`http://pplapi.com/country/${code}/random.json`)
+        .then(function(response){
+            return resolve(JSON.parse(response.body))
+        })
+    })
+}
+
 // Export defined functions.
 exports.getAgent = getAgent
+exports.getAgentByCountry = getAgentByCountry
